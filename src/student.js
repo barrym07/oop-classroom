fd5382d3457743249555c8ff23f2d6a627f15346
Barry <barrym07@gmail.com>
2021-09-17 09:51:13 -0600

    passes first 4 test of doHomework

--- src/student.js
+++ src/student.js
@@ -8,10 +8,21 @@ class Student {
   study() {
     if (this.skillLevel < 100) {
       this.skillLevel += 1;
-
     }
     return this;
+  }
 
+  doHomework(homework) {
+    if (homework.skillLevel > 1 || homework.skillLevel < 100 || homework.skillLevel != undefined) {
+      if (homework.skillLevel < this.skillLevel) {
+        homework.completed = true
+
+      }
+      else {
+        homework.completed = false;
+      }
+      return this.assignments.push(homework);
+    }
   }
 
 }
