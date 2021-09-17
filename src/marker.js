3dc4befec62ba8533b8d417ec98ed4182a7abe7a
Barry <barrym07@gmail.com>
2021-09-17 08:46:51 -0600

    Builds marker and passes first 2 test

--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,16 @@
 class Marker {
-  
+  constructor(size, color, remainingInk) {
+    this.size = size;
+    this.color = color;
+    this.remainingInk = remainingInk;
+
+  }
+  write(word) {
+    this.remainingInk = remainingInk - word.replace(/\s/g, '').length;
+    return word;
+
+  }
+
 }
 
 module.exports = Marker
