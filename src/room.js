93cbece23cce933bfbfca77d9f06d53f29c54034
Barry <barrym07@gmail.com>
2021-09-17 09:17:12 -0600

    passes all test for room

--- src/room.js
+++ src/room.js
@@ -4,6 +4,23 @@ class Room {
     this.description = description;
     this.contents = [];
   }
+  add(item) {
+    //changing this.contents to this.content makes it work
+    this.content = this.contents.push(item);
+    //returning this makes it chainable
+    return this;
+  }
+  has(item) {
+    for (let element of this.contents) {
+      if (element === item) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+  }
 
 }
 
